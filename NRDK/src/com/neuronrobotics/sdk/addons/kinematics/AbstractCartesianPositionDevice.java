3f40fcf54d6d2e91e53ec691d0229d91ca7a87a8
Adam Vadala-Roth <fenderstratman@gmail.com>
2011-07-09 06:09:56 +0000

    Most of the changes made now to the AbstractCartesianDevice abstract class were made on thursday but were not committed due to an error with subversion.

--- NRDK/src/com/neuronrobotics/sdk/addons/kinematics/AbstractCartesianPositionDevice.java
+++ NRDK/src/com/neuronrobotics/sdk/addons/kinematics/AbstractCartesianPositionDevice.java
@@ -5,4 +5,12 @@ public abstract class AbstractCartesianPositionDevice {
 	
 	public abstract void initialize();
 	public abstract void flush(double time);
+	public int ConvertUnitsToSteps(double units) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public abstract void SetXAxisPosition(int steps);
+	public abstract void SetYAxisPosition(int steps);
+	public abstract void SetZAxisPosition(int steps);
+	
 }
