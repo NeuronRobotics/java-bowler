cef4f7731961adfbf44c82374f0440d733a5a5db
Kevin Harrington <mad.hephaestus@gmail.com>
2012-06-05 17:13:32 +0000

    Bowler Camera System

---
+++ javasdk/NRSDK/src/com/neuronrobotics/sdk/addons/ros/RosDyIONode.java
@@ -0,0 +1,11 @@
+package com.neuronrobotics.sdk.addons.ros;
+
+import com.neuronrobotics.sdk.dyio.DyIO;
+
+public class RosDyIONode {
+	private final DyIO dyio;
+	public RosDyIONode(DyIO d){
+		dyio=d;
+		
+	}
+}
