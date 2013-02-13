5c9edeb187fbc4cb9427b741d40fcd4a89fd920d
Kevin Harrington <mad.hephaestus@gmail.com>
2013-02-13 17:12:19 +0000

    dyio namespace tester

---
+++ test/java/src/com/neuronrobotics/test/dyio/DyIONamespaceTester.java
@@ -0,0 +1,37 @@
+package com.neuronrobotics.test.dyio;
+
+import com.neuronrobotics.sdk.dyio.DyIO;
+import com.neuronrobotics.sdk.ui.ConnectionDialog;
+
+public class DyIONamespaceTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DyIO dyio=new DyIO();
+		dyio.enableDebug();
+		if (!ConnectionDialog.getBowlerDevice(dyio)){
+			System.exit(1);
+		}
+		dyio.enableDebug();
+		
+		dyio.getRevisions();
+		
+		String name = dyio.getInfo();
+		
+		dyio.setInfo("My DyIO");
+		
+		String newName = dyio.getInfo();
+		
+		dyio.setInfo(name);
+		
+		double volts = dyio.getBatteryVoltage(true);
+		
+		System.out.println("Name was: "+name+" set to "+newName);
+		System.out.println("Set to "+newName);
+		System.out.println("Voltage = "+volts+" bank A = "+dyio.getBankAState()+" bank B = "+dyio.getBankBState());
+		System.exit(0);
+	}
+
+}
