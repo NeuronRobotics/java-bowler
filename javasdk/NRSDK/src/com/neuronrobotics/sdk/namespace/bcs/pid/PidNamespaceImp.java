9a58651a67fc2b066b6b6c90eac5e32c5ed247cc
Kevin Harrington <mad.hephaestus@gmail.com>
2013-06-26 18:34:37 +0000

    Added all elements of the PID namespace

--- javasdk/NRSDK/src/com/neuronrobotics/sdk/namespace/bcs/pid/PidNamespaceImp.java
+++ javasdk/NRSDK/src/com/neuronrobotics/sdk/namespace/bcs/pid/PidNamespaceImp.java
@@ -3,102 +3,150 @@ package com.neuronrobotics.sdk.namespace.bcs.pid;
 import com.neuronrobotics.sdk.common.BowlerAbstractDevice;
 import com.neuronrobotics.sdk.common.BowlerDatagram;
 import com.neuronrobotics.sdk.common.BowlerMethod;
+import com.neuronrobotics.sdk.common.ByteList;
 import com.neuronrobotics.sdk.common.DeviceConnectionException;
 import com.neuronrobotics.sdk.pid.PDVelocityConfiguration;
 import com.neuronrobotics.sdk.pid.PIDChannel;
 import com.neuronrobotics.sdk.pid.PIDCommandException;
 import com.neuronrobotics.sdk.pid.PIDConfiguration;
+import com.neuronrobotics.sdk.pid.PIDEvent;
+import com.neuronrobotics.sdk.pid.PIDLimitEvent;
 
 public class PidNamespaceImp extends GenericPidNamespaceImp {
 	private final String ns = "bcs.pid.*";
+	
+	
+	
 	public PidNamespaceImp(BowlerAbstractDevice device) {
 		super(device);
+	}
+	
+	private Object[] send(BowlerMethod method, String rpcString, Object[] arguments){
+		return getDevice().send(ns,method,rpcString,arguments);
 	}
 
 	@Override
 	public boolean ResetPIDChannel(int group, int valueToSetCurrentTo) throws DeviceConnectionException {
 		Object[] args = new Object[]{group,valueToSetCurrentTo};
-		getDevice().send(ns,BowlerMethod.POST,"rpid",args);
+		send(BowlerMethod.POST,
+				"rpid",
+				args);
 		return true;
 	}
 
 	@Override
 	public boolean ConfigurePIDController(PIDConfiguration config) {		
-		getDevice().send(ns,BowlerMethod.CRITICAL,"cpid",config.getArgs());
+		send(BowlerMethod.CRITICAL,
+				"cpid",
+				config.getArgs());
 		return true;
 	}
 
 	@Override
 	public PIDConfiguration getPIDConfiguration(int group) {
 		
-		return new PIDConfiguration(getDevice().send(ns,BowlerMethod.GET,"cpid",new Object[]{group}));
+		return new PIDConfiguration(send(BowlerMethod.GET,
+				"cpid",
+				new Object[]{group}));
 	}
 
 	@Override
 	public boolean ConfigurePDVelovityController(PDVelocityConfiguration config) {
-		getDevice().send(ns,BowlerMethod.CRITICAL,"cpdv",config.getArgs());
+		send(BowlerMethod.CRITICAL,
+				"cpdv",
+				config.getArgs());
 		return true;
 	}
 
 	@Override
 	public PDVelocityConfiguration getPDVelocityConfiguration(int group) {
-		Object [] args = getDevice().send(ns,BowlerMethod.GET,"cpdv",new Object[]{group});
+		Object [] args = send(BowlerMethod.GET,
+				"cpdv",
+				new Object[]{group});
 		return new PDVelocityConfiguration(args);
 	}
 
 	@Override
 	public int getPIDChannelCount() {
-		Object [] args = getDevice().send(ns,BowlerMethod.GET,"gpdc",new Object[]{});
+		Object [] args = send(BowlerMethod.GET,
+				"gpdc",
+				new Object[]{});
 		return (Integer)args[0];
 	}
 
 	@Override
 	public boolean SetPIDSetPoint(int group, int setpoint, double seconds) {
-		getDevice().send(ns,BowlerMethod.POST,"_pid",new Object[]{group,setpoint,seconds});
+		send(BowlerMethod.POST,
+				"_pid",
+				new Object[]{	group,
+								setpoint,
+								(int)(seconds*1000)});
 		return true;
 	}
 
 	@Override
 	public boolean SetAllPIDSetPoint(int[] setpoints, double seconds) {
-		getDevice().send(ns,BowlerMethod.POST,"apid",new Object[]{seconds,setpoints});
+		send(BowlerMethod.POST,
+				"apid",
+				new Object[]{	(int)(seconds*1000),
+								setpoints});
 		return false;
 	}
 
 	@Override
 	public int GetPIDPosition(int group) {
-		Object [] args = getDevice().send(ns,BowlerMethod.GET,"_pid",new Object[]{group});
+		Object [] args = send(BowlerMethod.GET,
+				"_pid",
+				new Object[]{group});
 		return (Integer)args[0];
 	}
 
 	@Override
 	public int[] GetAllPIDPosition() {
-		// TODO Auto-generated method stub
-		return null;
+		Object [] args = send(BowlerMethod.GET,
+				"apid",
+				new Object[]{});
+		return (int [])args[0];
 	}
 
 	@Override
-	public boolean SetPDVelocity(int group, int unitsPerSecond, double seconds)
-			throws PIDCommandException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public PIDChannel getPIDChannel(int group) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean SetPDVelocity(int group, int unitsPerSecond, double seconds)throws PIDCommandException {
+		send(BowlerMethod.POST,
+				"_vpd",
+				new Object[]{	group,
+								unitsPerSecond,
+								(int)(seconds*1000)});
+		return true;
 	}
 
 	@Override
 	public boolean killAllPidGroups() {
-		// TODO Auto-generated method stub
-		return false;
+		send(BowlerMethod.CRITICAL,
+				"kpid",
+				new Object[]{});
+		return true;
 	}
 
 	@Override
 	public void onAsyncResponse(BowlerDatagram data) {
-		// TODO Auto-generated method stub
-		
+		//Log.debug("\nPID ASYNC<<"+data);
+		if(data.getRPC().contains("_pid")){
+			
+			PIDEvent e =new PIDEvent(data);
+	
+			firePIDEvent(e);
+		}
+		if(data.getRPC().contains("apid")){
+			int [] pos = new int[getNumberOfChannels()];
+			for(int i=0;i<getNumberOfChannels();i++) {
+				pos[i] = ByteList.convertToInt( data.getData().getBytes(i*4, 4),true);
+				PIDEvent e =new PIDEvent(i,pos[i],System.currentTimeMillis(),0);
+				firePIDEvent(e);
+			}	
+		}
+		if(data.getRPC().contains("pidl")){
+			firePIDLimitEvent(new PIDLimitEvent(data));
+		}
 	}
 	
 }
