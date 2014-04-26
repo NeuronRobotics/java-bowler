a85b5e1b03ccb91289d6dfd41401387394da1cb7
kharrington <kharrington@neuronrobotics.com>
2014-04-26 14:17:21 -0400

    Gcode unit test

--- test/java/src/junit/test/neuronrobotics/cartesian/CartesianTestSuit.java
+++ test/java/src/junit/test/neuronrobotics/cartesian/CartesianTestSuit.java
@@ -9,6 +9,7 @@ import com.neuronrobotics.replicator.driver.NRPrinter;
 import com.neuronrobotics.sdk.common.Log;
 import com.neuronrobotics.sdk.serial.SerialConnection;
 import com.neuronrobotics.sdk.ui.ConnectionDialog;
+import com.neuronrobotics.sdk.util.ThreadUtil;
 
 public class CartesianTestSuit {
 
@@ -17,11 +18,19 @@ public class CartesianTestSuit {
 		Log.enableDebugPrint();
 		DeltaForgeDevice dev = new DeltaForgeDevice();
 		dev.setConnection(new SerialConnection("/dev/BowlerDevice.74F726000000"));
+		dev.getConnection().setSynchronusPacketTimeoutTime(2000);
 		assertTrue(dev.connect());
 		System.out.println("Connection ok");
 		
 		NRPrinter printer = new NRPrinter(dev);
-		
+		printer.cancelPrint();
+		ThreadUtil.wait(5000);
+		try{
+			printer.print(CartesianTestSuit.class.getResourceAsStream("test.gcode"));
+		}catch(Exception ex){
+			ex.printStackTrace();
+			fail();
+		}
 		
 		
 	}
