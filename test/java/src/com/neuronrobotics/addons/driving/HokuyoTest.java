94f202e9bdfc52b92b948c32313688b8114f32b2
kharrington <kharrington@neuronrobotics.com>
2014-05-01 10:16:58 -0400

    Adding the direct control framework to tests

--- test/java/src/com/neuronrobotics/addons/driving/HokuyoTest.java
+++ test/java/src/com/neuronrobotics/addons/driving/HokuyoTest.java
@@ -7,11 +7,13 @@ import gnu.io.NRSerialPort;
 public class HokuyoTest implements ISensorListener {
 	
 	private SimpleDisplay display = new SimpleDisplay();
-	
+	private long start;
 	private HokuyoTest(){
-		LaserRangeSensor range = new LaserRangeSensor(new NRSerialPort("/dev/ttyACM0", 115200));
+		LaserRangeSensor range = new LaserRangeSensor(new NRSerialPort("/dev/ttyACM1", 115200));
 		range.addSensorListener(this);
+		start=System.currentTimeMillis();
 		range.StartSweep(-90, 90, .5);
+		
 //		try {
 //			Thread.sleep(10000);
 //		} catch (InterruptedException e) {
@@ -26,9 +28,11 @@ public class HokuyoTest implements ISensorListener {
 
 	@Override
 	public void onRangeSensorEvent(AbstractSensor source,ArrayList<DataPoint> data, long timeStamp) {
-		//System.out.println("Range Sensor Event "+data);
+		System.out.println("Range Sensor Event "+(System.currentTimeMillis()-start));
 		display.setData(data);
+		start=System.currentTimeMillis();
 		source.StartSweep(-90, 90, .5);
+		
 	}
 
 	@Override
