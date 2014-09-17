90263ca6514c4dd789194dde228fd5bd73d40382
madhephaestus <mad.hephaestus@gmail.com>
2014-09-17 13:54:56 -0400

    both uinput and out put tests

--- test/java/src/junit/test/neuronrobotics/namespace/DyIONamespaceTest.java
+++ test/java/src/junit/test/neuronrobotics/namespace/DyIONamespaceTest.java
@@ -16,8 +16,8 @@ import com.neuronrobotics.sdk.serial.SerialConnection;
 
 public class DyIONamespaceTest {
 	
-	private static DyIO tester=null;
-	private static DyIO target=null;
+	private static DyIO harness=null;
+	private static DyIO testDevice=null;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -26,31 +26,32 @@ public class DyIONamespaceTest {
 		
 		SerialConnection testerConection = SerialConnection.getConnectionByMacAddress(new MACAddress("74:F7:26:80:00:7C"));
 		assertTrue(testerConection!=null);
-		tester = new DyIO(testerConection);
-		tester.connect();
+		harness = new DyIO(testerConection);
+		harness.connect();
 		
 		SerialConnection targetConection = SerialConnection.getConnectionByMacAddress(new MACAddress("74:F7:26:00:00:00"));
 		assertTrue(targetConection!=null);
-		targetConection.setSynchronusPacketTimeoutTime(5000);
-		target = new DyIO(targetConection);
-		target.connect();
+		targetConection.setSynchronusPacketTimeoutTime(10000);
+		testDevice = new DyIO(targetConection);
+		testDevice.connect();
 		
 		
 	}
 	
 	@After
 	public void shutdownDevices(){
-		target.disconnect();
-		if(tester!=null)
-			tester.disconnect();
+		
+		testDevice.disconnect();
+		if(harness!=null)
+			harness.disconnect();
 	}
 	
 	@Test public void DyIONameTest(){
 
-		if(!target.isAvailable())
+		if(!testDevice.isAvailable())
 			fail();
 		
-		String name = target.getInfo();
+		String name = testDevice.getInfo();
 		
 		String setName;
 		if(name.contains("My DyIO"))
@@ -58,34 +59,34 @@ public class DyIONamespaceTest {
 		else
 			setName="My DyIO";
 		
-		target.setInfo(setName);
+		testDevice.setInfo(setName);
 		
-		String newName = target.getInfo();
+		String newName = testDevice.getInfo();
 		
 
-		target.setInfo(name);
+		testDevice.setInfo(name);
 	
 		assertTrue(setName.contains(newName));
-		assertTrue(name.contains(target.getInfo()));
-		assertTrue(target.ping() );
+		assertTrue(name.contains(testDevice.getInfo()));
+		assertTrue(testDevice.ping() );
 
 	}
 	
 	@Test public void DyIOPowerTest(){
-		if(!target.isAvailable())
+		if(!testDevice.isAvailable())
 			fail();
-		double volts = target.getBatteryVoltage(true);
+		double volts = testDevice.getBatteryVoltage(true);
 		
-		target.setServoPowerSafeMode(true);
+		testDevice.setServoPowerSafeMode(true);
 
 
 	}
 	
 	@Test public void DyIOModesTest(){
-		if(!target.isAvailable())
+		if(!testDevice.isAvailable())
 			fail();
 
-		ArrayList<DyIOChannelMode> modes = target.getAllChannelModes();
+		ArrayList<DyIOChannelMode> modes = testDevice.getAllChannelModes();
 		for(int i=0;i<modes.size();i++){
 			
 			if(modes.get(i)==DyIOChannelMode.DIGITAL_IN){
@@ -93,33 +94,62 @@ public class DyIONamespaceTest {
 			}else{
 				modes.set(i, DyIOChannelMode.DIGITAL_IN);
 			}
-			target.setMode(i, modes.get(i));
+			testDevice.setMode(i, modes.get(i));
 		}
 		
-		ArrayList<DyIOChannelMode> modesAfter = target.getAllChannelModes();
+		ArrayList<DyIOChannelMode> modesAfter = testDevice.getAllChannelModes();
 		for(int i=0;i<modes.size();i++){
 			assertTrue(modes.get(i)==modesAfter.get(i));
-			assertTrue(modes.get(i)==target.getMode(i));
+			assertTrue(modes.get(i)==testDevice.getMode(i));
 		}
 	}
-	
-	@Test public void DyIOValuesTest(){
-		if(!target.isAvailable() || tester == null)
+	@Test public void DyIOInputTest(){
+		if(!testDevice.isAvailable() || harness == null)
 			fail();
-		int numPins = target.getDyIOChannelCount();
+		int numPins = testDevice.getDyIOChannelCount();
+		
+		//Test device as input
 		for(int i=0;i<numPins;i++){
-			int testerIndex = numPins-1-i;
-			tester.setMode(testerIndex, DyIOChannelMode.DIGITAL_OUT);
-			target.setMode(i, DyIOChannelMode.DIGITAL_IN);
-			
-			boolean state=true;
-			for(int j=0;j<5;j++){
-				int pinState = state?1:0;
-				tester.setValue(testerIndex, pinState);
-				int gotValue = target.getValue(i);
-				System.out.println(" Pin:"+i+" Tester:"+testerIndex+" setting to: "+pinState+" got:"+gotValue);
-				assertTrue(gotValue==pinState);
-				state = !state;
+			if(!(i==16 || i==17)){
+				int testerIndex = numPins-1-i;
+				harness.setMode(testerIndex, DyIOChannelMode.DIGITAL_OUT);
+				testDevice.setMode(i, DyIOChannelMode.DIGITAL_IN);
+				
+				boolean state=true;
+				for(int j=0;j<5;j++){
+					int pinState = state?1:0;
+					harness.setValue(testerIndex, pinState);
+					int gotValue = testDevice.getValue(i);
+					System.out.println(" Pin:"+i+" Tester:"+testerIndex+" setting to: "+pinState+" got:"+gotValue);
+					assertTrue(gotValue==pinState);
+					state = !state;
+				}
+			}
+		}
+		
+	}
+	
+	@Test public void DyIOOutputTest(){
+		if(!testDevice.isAvailable() || harness == null)
+			fail();
+		int numPins = testDevice.getDyIOChannelCount();
+
+		//test device as output
+		for(int i=0;i<numPins;i++){
+			if(!(i==16 || i==17)){
+				int testerIndex = numPins-1-i;
+				harness.setMode(testerIndex, DyIOChannelMode.DIGITAL_IN);
+				testDevice.setMode(i, DyIOChannelMode.DIGITAL_OUT);
+				
+				boolean state=true;
+				for(int j=0;j<5;j++){
+					int pinState = state?1:0;
+					testDevice.setValue(i, pinState);
+					int gotValue = harness.getValue(testerIndex);
+					System.out.println(" Pin:"+i+" Tester:"+testerIndex+" setting to: "+pinState+" got:"+gotValue);
+					assertTrue(gotValue==pinState);
+					state = !state;
+				}
 			}
 		}
 		
@@ -127,10 +157,10 @@ public class DyIONamespaceTest {
 
 	@Test
 	public void dyioNamespaceTest() {
-		if(!target.isAvailable())
+		if(!testDevice.isAvailable())
 			fail();
-		assertTrue(target.hasNamespace("neuronrobotics.dyio.*"));
-		target.getRevisions();
+		assertTrue(testDevice.hasNamespace("neuronrobotics.dyio.*"));
+		testDevice.getRevisions();
 	
 	}
 
