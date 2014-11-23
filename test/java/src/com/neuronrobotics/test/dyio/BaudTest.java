0f9b77a8ada1ddd38f7c8df0eee6aa3e0264a3cf
madhephaestus <mad.hephaestus@gmail.com>
2014-11-23 14:06:54 -0500

    allowing negative percentages and printing the BRG value

--- test/java/src/com/neuronrobotics/test/dyio/BaudTest.java
+++ test/java/src/com/neuronrobotics/test/dyio/BaudTest.java
@@ -4,17 +4,9 @@ import java.util.ArrayList;
 
 public class BaudTest {
 	
-	public static double almostEqual(double a, double b){
-		double absoluteDifference = Math.abs(a-b);
-		double percent = (absoluteDifference/a)*100;
-	    return percent;
-	}
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		ArrayList<Double> picBauds = new ArrayList<Double>();
-		ArrayList<Double> avrBauds = new ArrayList<Double>();
+	public BaudTest(){
+		ArrayList<BaudObject> picBauds = new ArrayList<BaudObject>();
+		ArrayList<BaudObject> avrBauds = new ArrayList<BaudObject>();
 		
 		for(int i=0xffff;i>0;i--){
 			double tmp = (80000000.0/(4.0*(((double)i)+1.0)));
@@ -22,7 +14,7 @@ public class BaudTest {
 			int calculated = (int) ((80000000.0/(4.0*tmp))-1.0);
 			if(calculated == i&& tmp>10000){
 				//System.out.println("Pic Baud = "+tmp+" baud Set = "+calculated);
-				picBauds.add(tmp);
+				picBauds.add(new BaudObject(i, tmp));
 			}
 		}
 		
@@ -33,19 +25,65 @@ public class BaudTest {
 			int calculated =(int) (( 18432000.0/(16.0*tmp))-1.0);
 			if(calculated == i && tmp>10000){
 				//System.out.println("AVR Baud = "+tmp+" baud Set = "+calculated);
-				avrBauds.add(tmp);
+				avrBauds.add(new BaudObject(i, tmp));
 			}
 		}
 		
-		for(Double p:picBauds ){
-			for(Double a: avrBauds){
-				double percent = almostEqual( a, p);
-				if(percent < .2){
-					System.out.println("AVR Baud = "+a.intValue()+" \tPic Baud = "+p.intValue()+ "\tPercent = "+percent);
+		for(BaudObject p:picBauds ){
+			for(BaudObject a: avrBauds){
+				double percent = almostEqual( a.getBaudrate(), p.getBaudrate());
+				if(percent < .1 && percent > -1 ){
+					System.out.print("\r\nAVR Baud = "+a.intValue()+" \tPic Baud = "+p.intValue()+ "\tPercent = "+percent);
+					System.out.print( " AVR Baud = "+a.getbRGValue()+" \tPic Baud = "+p.getbRGValue());
 				}
 			}
 		}
-		
+	}
+	
+	public static double almostEqual(double a, double b){
+		double absoluteDifference = (a-b);
+		double percent = (absoluteDifference/a)*100;
+	    return percent;
+	}
+	
+	private class BaudObject{
+		private int bRGValue;
+		private double baudrate;
+
+		BaudObject(int BRGValue, double baudrate){
+			setbRGValue(BRGValue);
+			this.setBaudrate(baudrate);
+			
+		}
+
+		public int getbRGValue() {
+			return bRGValue;
+		}
+
+		public void setbRGValue(int bRGValue) {
+			this.bRGValue = bRGValue;
+		}
+
+		public double getBaudrate() {
+			return baudrate;
+		}
+
+		public void setBaudrate(double baudrate) {
+			this.baudrate = baudrate;
+		}
+
+		public int intValue() {
+			// TODO Auto-generated method stub
+			return new Double(getBaudrate()).intValue();
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		new  BaudTest();
 				
 	}
 
