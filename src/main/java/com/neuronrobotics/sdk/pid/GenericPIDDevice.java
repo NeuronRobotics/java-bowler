213de7cd914553929ad83503fa164988ea0b90d8
NIrav Patel <nap_38ce@yahoo.com>
2015-07-29 17:11:01 -0400

    Added method which calls getPIDChannelCount() to return total number of PID channels. This is for backward compatibility.

--- src/main/java/com/neuronrobotics/sdk/pid/GenericPIDDevice.java
+++ src/main/java/com/neuronrobotics/sdk/pid/GenericPIDDevice.java
@@ -85,7 +85,11 @@ public class GenericPIDDevice extends BowlerAbstractDevice implements IExtendedP
 	public int getPIDChannelCount() {
 		return getImplementation().getNumberOfChannels();
 	}
-
+	
+	//This is added for backward compatibility
+	public int getNumberOfChannels(){
+		return getPIDChannelCount();
+	}
 	@Override
 	public boolean SetPIDSetPoint(int group, int setpoint, double seconds) {
 		return getImplementation().SetPIDSetPoint(group, setpoint, seconds);
