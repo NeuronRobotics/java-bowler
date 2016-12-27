c7877f16ce9cb90974b3721af45a4436f37d1c99
Kevin Harrington <mad.hephaestus@gmail.com>
2016-12-27 13:01:02 -0500

    Fixing the bounding in my internal computations.

--- src/main/java/com/neuronrobotics/sdk/addons/kinematics/math/RotationNR.java
+++ src/main/java/com/neuronrobotics/sdk/addons/kinematics/math/RotationNR.java
@@ -1,11 +1,8 @@
 package com.neuronrobotics.sdk.addons.kinematics.math;
 
-import Jama.Matrix;
-import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
-import org.apache.commons.math3.geometry.euclidean.threed.RotationConvention;
-import org.apache.commons.math3.geometry.euclidean.threed.RotationOrder;
-
 import com.neuronrobotics.sdk.common.Log;
+
+import Jama.Matrix;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -19,10 +16,8 @@ import com.neuronrobotics.sdk.common.Log;
 public class RotationNR {
 
 	/** The rotation matrix. */
-	//double[][] rotationMatrix = ;
-	private Rotation storage=new Rotation(1,0,0,0,false);
-	private static RotationOrder order = RotationOrder.XYZ;
-	private static RotationConvention convention = RotationConvention.VECTOR_OPERATOR;
+	double[][] rotationMatrix = new double[][] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+
 	/**
 	 * Null constructor forms a.
 	 */
@@ -42,18 +37,17 @@ public class RotationNR {
 	// create a new object with the given simplified rotations
 	public RotationNR(double tilt, double azumeth, double elevation) {
 		if (Double.isNaN(tilt))
-			throw new RuntimeException("Value can not be NaN");
+			throw new NumberFormatException("Value can not be NaN");
 		if (Double.isNaN(azumeth))
-			throw new RuntimeException("Value can not be NaN");
+			throw new NumberFormatException("Value can not be NaN");
 		if (Double.isNaN(elevation))
-			throw new RuntimeException("Value can not be NaN");
-		if(elevation >90 || elevation <-90){
-			throw new RuntimeException("Elevation can not be greater than 90 nor less than -90");
-		}
+			throw new NumberFormatException("Value can not be NaN");
+		if (elevation >= 90 || elevation <= -90)
+			throw new NumberFormatException("Elevation must be between 90 and -90");
 		loadFromAngles(tilt, azumeth, elevation);
 		if (Double.isNaN(getRotationMatrix2QuaturnionW()) || Double.isNaN(getRotationMatrix2QuaturnionX())
 				|| Double.isNaN(getRotationMatrix2QuaturnionY()) || Double.isNaN(getRotationMatrix2QuaturnionZ())) {
-			Log.error("Failing to set proper angle, jittering");
+			// System.err.println("Failing to set proper angle, jittering");
 			loadFromAngles(tilt + Math.random() * .02 + .001, azumeth + Math.random() * .02 + .001,
 					elevation + Math.random() * .02 + .001);
 		}
@@ -61,11 +55,29 @@ public class RotationNR {
 	}
 
 	private void loadFromAngles(double tilt, double azumeth, double elevation) {
-		storage = new Rotation(getOrder(), getConvention(), 
-				Math.toRadians(tilt), 
-				Math.toRadians(elevation), 
-				Math.toRadians(azumeth)
-				);
+		double attitude = Math.toRadians(elevation);
+		double heading = Math.toRadians(azumeth);
+		double bank = Math.toRadians(tilt);
+		double w, x, y, z;
+		// Assuming the angles are in radians.
+		double c1 = Math.cos(heading / 2);
+		// if(Double.isNaN(c1))
+		//
+		double s1 = Math.sin(heading / 2);
+		double c2 = Math.cos(attitude / 2);
+		double s2 = Math.sin(attitude / 2);
+		double c3 = Math.cos(bank / 2);
+		double s3 = Math.sin(bank / 2);
+		double c1c2 = c1 * c2;
+		double s1s2 = s1 * s2;
+		// System.out.println("C1 ="+c1+" S1 ="+s1+" |C2 ="+c2+" S2 ="+s2+" |C3
+		// ="+c3+" S3 ="+s3);
+		w = c1c2 * c3 - s1s2 * s3;
+		x = c1c2 * s3 + s1s2 * c3;
+		y = s1 * c2 * c3 + c1 * s2 * s3;
+		z = c1 * s2 * c3 - s1 * c2 * s3;
+		// System.out.println("W ="+w+" x ="+x+" y ="+y+" z ="+z);
+		quaternion2RotationMatrix(w, x, y, z);
 	}
 
 	/**
@@ -216,7 +228,14 @@ public class RotationNR {
 				throw new RuntimeException("Must be 3x3 rotation matrix");
 			}
 		}
-		storage = new Rotation(rotM, 1.0);
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				// if(rotM[i][j]>1){
+				// rotM[i][j]=0;//normalization
+				// }
+				rotationMatrix[i][j] = rotM[i][j];
+			}
+		}
 	}
 
 	/**
@@ -225,8 +244,13 @@ public class RotationNR {
 	 * @return the rotation matrix
 	 */
 	public double[][] getRotationMatrix() {
-		
-		return storage.getMatrix();
+		double[][] b = new double[3][3];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				b[i][j] = rotationMatrix[i][j];
+			}
+		}
+		return b;
 	}
 
 	/*
@@ -247,8 +271,8 @@ public class RotationNR {
 		}
 		s += "]";
 		return "Quaturnion: " + "W=" + getRotationMatrix2QuaturnionW() + ", " + "x=" + getRotationMatrix2QuaturnionX()
-				+ ", " + "y=" + getRotationMatrix2QuaturnionY() + ", " + "z=" + getRotationMatrix2QuaturnionZ() + "\n"
-				+ "Rotation angle (degrees): " + "az=" + getRotationAzimuth() + ", " + "elev=" + getRotationElevation() + ", " + " tilt="
+				+ ", " + "y=" + getRotationMatrix2QuaturnionY() + ", " + "z=" + getRotationMatrix2QuaturnionZ() + "\t"
+				+ "Rotation angle (degrees): " + "Azimuth=" + getRotationAzimuth() + ", " + "Elevation=" + getRotationElevation() + ", " + "Tilt="
 				+ getRotationTilt() + "";
 	}
 
@@ -287,17 +311,153 @@ public class RotationNR {
 	 */
 	protected void quaternion2RotationMatrix(double w, double x, double y, double z) {
 		if (Double.isNaN(w))
-			throw new RuntimeException("Value can not be NaN");
+			throw new NumberFormatException("Value can not be NaN");
 		if (Double.isNaN(x))
-			throw new RuntimeException("Value can not be NaN");
+			throw new NumberFormatException("Value can not be NaN");
 		if (Double.isNaN(y))
-			throw new RuntimeException("Value can not be NaN");
+			throw new NumberFormatException("Value can not be NaN");
 		if (Double.isNaN(z))
-			throw new RuntimeException("Value can not be NaN");
-		storage = new Rotation(w, x, y,z, true);
+			throw new NumberFormatException("Value can not be NaN");
+		double norm = Math.sqrt(w * w + x * x + y * y + z * z);
+		// we explicitly test norm against one here, saving a division
+		// at the cost of a test and branch. Is it worth it?
+		double s = (norm == 1f) ? 2f : (norm > 0f) ? 2f / norm : 0;
+		// compute xs/ys/zs first to save 6 multiplications, since xs/ys/zs
+		// will be used 2-4 times each.
+		double xs = x * s;
+		double ys = y * s;
+		double zs = z * s;
+		double xx = x * xs;
+		double xy = x * ys;
+		double xz = x * zs;
+		double xw = w * xs;
+		double yy = y * ys;
+		double yz = y * zs;
+		double yw = w * ys;
+		double zz = z * zs;
+		double zw = w * zs;
+
+		// using s=2/norm (instead of 1/norm) saves 9 multiplications by 2 here
+		rotationMatrix[0][0] = 1 - (yy + zz);
+		rotationMatrix[0][1] = (xy - zw);
+		rotationMatrix[0][2] = (xz + yw);
+
+		rotationMatrix[1][0] = (xy + zw);
+		rotationMatrix[1][1] = 1 - (xx + zz);
+		rotationMatrix[1][2] = (yz - xw);
+
+		rotationMatrix[2][0] = (xz - yw);
+		rotationMatrix[2][1] = (yz + xw);
+		rotationMatrix[2][2] = 1 - (xx + yy);
+
+		toString(rotationMatrix);
 	}
 
-	
+	// /**
+	// * This requires a pure rotation matrix 'm' as input. from
+	// *
+	// http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToAngle/
+	// *
+	// * @return the double[]
+	// */
+	// public double[] toAxisAngle() {
+	// double angle, x, y, z; // variables for result
+	// double epsilon = 0.01; // margin to allow for rounding errors
+	// double epsilon2 = 0.1; // margin to distinguish between 0 and 180
+	// // degrees
+	// // optional check that input is pure rotation, 'isRotationMatrix' is
+	// // defined at:
+	// //
+	// http://www.euclideanspace.com/maths/algebra/matrix/orthogonal/rotation/
+	// if (((Math.abs(rotationMatrix[0][1]) - Math.abs(rotationMatrix[1][0])) <
+	// epsilon)
+	// && ((Math.abs(rotationMatrix[0][2]) - Math.abs(rotationMatrix[2][0])) <
+	// epsilon)
+	// && ((Math.abs(rotationMatrix[1][2]) - Math.abs(rotationMatrix[2][1])) <
+	// epsilon)) {
+	// // singularity found
+	// // first check for identity matrix which must have +1 for all terms
+	// // in leading diagonaland zero in other terms
+	// if ((Math.abs(rotationMatrix[0][1]) + Math.abs(rotationMatrix[1][0])) <
+	// epsilon2
+	// && (Math.abs(rotationMatrix[0][2]) + Math.abs(rotationMatrix[2][0])) <
+	// epsilon2
+	// && (Math.abs(rotationMatrix[1][2]) + Math.abs(rotationMatrix[2][1])) <
+	// epsilon2
+	// && (Math.abs(rotationMatrix[0][0]) + Math.abs(rotationMatrix[1][1]) +
+	// Math.abs(rotationMatrix[2][2])
+	// - 3) < epsilon2) {
+	// // this singularity is identity matrix so angle = 0
+	// return new double[] { 0, 1, 0, 0 }; // zero angle, arbitrary
+	// // axis
+	// }
+	// // otherwise this singularity is angle = 180
+	// angle = Math.PI;
+	// double xx = (rotationMatrix[0][0] + 1) / 2;
+	// double yy = (rotationMatrix[1][1] + 1) / 2;
+	// double zz = (rotationMatrix[2][2] + 1) / 2;
+	// double xy = (rotationMatrix[0][1] + rotationMatrix[1][0]) / 4;
+	// double xz = (rotationMatrix[0][2] + rotationMatrix[2][0]) / 4;
+	// double yz = (rotationMatrix[1][2] + rotationMatrix[2][1]) / 4;
+	// if ((xx > yy) && (xx > zz)) { // m[0][0] is the largest diagonal
+	// // term
+	// if (xx < epsilon) {
+	// x = 0;
+	// y = 0.7071;
+	// z = 0.7071;
+	// } else {
+	// x = Math.sqrt(xx);
+	// y = xy / x;
+	// z = xz / x;
+	// }
+	// } else if (yy > zz) { // m[1][1] is the largest diagonal term
+	// if (yy < epsilon) {
+	// x = 0.7071;
+	// y = 0;
+	// z = 0.7071;
+	// } else {
+	// y = Math.sqrt(yy);
+	// x = xy / y;
+	// z = yz / y;
+	// }
+	// } else { // m[2][2] is the largest diagonal term so base result on
+	// // this
+	// if (zz < epsilon) {
+	// x = 0.7071;
+	// y = 0.7071;
+	// z = 0;
+	// } else {
+	// z = Math.sqrt(zz);
+	// x = xz / z;
+	// y = yz / z;
+	// }
+	// }
+	// return new double[] { angle, x, y, z }; // return 180 deg rotation
+	// }
+	// // as we have reached here there are no singularities so we can handle
+	// // normally
+	// double s = Math
+	// .sqrt((rotationMatrix[2][1] - rotationMatrix[1][2]) *
+	// (rotationMatrix[2][1] - rotationMatrix[1][2])
+	// + (rotationMatrix[0][2] - rotationMatrix[2][0]) * (rotationMatrix[0][2] -
+	// rotationMatrix[2][0])
+	// + (rotationMatrix[1][0] - rotationMatrix[0][1])
+	// * (rotationMatrix[1][0] - rotationMatrix[0][1])); // used
+	// // to
+	// // normalise
+	// if (Math.abs(s) < 0.001)
+	// s = 1;
+	// // prevent divide by zero, should not happen if matrix is orthogonal and
+	// // should be
+	// // caught by singularity test above, but I've left it in just in case
+	// angle = Math.acos((rotationMatrix[0][0] + rotationMatrix[1][1] +
+	// rotationMatrix[2][2] - 1) / 2);
+	// x = (rotationMatrix[2][1] - rotationMatrix[1][2]) / s;
+	// y = (rotationMatrix[0][2] - rotationMatrix[2][0]) / s;
+	// z = (rotationMatrix[1][0] - rotationMatrix[0][1]) / s;
+	// return new double[] { angle, x, y, z };
+	// }
+
 	/**
 	 * Bound.
 	 *
@@ -313,9 +473,79 @@ public class RotationNR {
 		return n >= low && n <= high;
 	}
 
+	/**
+	 * Gets the rot angle.
+	 *
+	 * @param index
+	 *            the index
+	 * @return the rot angle
+	 */
+	private double getRotAngle(int index) {
+		double w, x, y, z, tilt, azumiuth, elevation;
+		w = getRotationMatrix2QuaturnionW();
+		x = getRotationMatrix2QuaturnionX();
+		y = getRotationMatrix2QuaturnionY();
+		z = getRotationMatrix2QuaturnionZ();
+		double sqw = w * w;
+		double sqx = x * x;
+		double sqy = y * y;
+		double sqz = z * z;
+		double unit = sqx + sqy + sqz + sqw; // if normalised is one, otherwise
+												// is correction factor
+		double test = x * y + z * w;
+		double testingValue = (0.5 - Double.MIN_VALUE) * unit;// this is a far
+																// more robust
+																// bound
+																// checking
+																// using the
+																// min value of
+																// the data type
+		if (test > testingValue) { // singularity at north pole
+			Log.warning("North pole singularity ");
+			azumiuth = 2 * Math.atan2(x, w);
+			elevation = Math.PI / 2;
+			tilt = 0;
 
+		} else if (test < -testingValue) { // singularity at south pole
+			Log.warning("South pole singularity");
+			azumiuth = -2 * Math.atan2(x, w);
+			elevation = -Math.PI / 2;
+			tilt = 0;
 
-	
+		} else {
+			azumiuth = Math.atan2(2 * y * w - 2 * x * z, sqx - sqy - sqz + sqw);
+			elevation = Math.asin(2 * test / unit);
+			tilt = Math.atan2(2 * x * w - 2 * y * z, -sqx + sqy - sqz + sqw);
+		}
+
+		switch (index) {
+		case 0:
+			return tilt;
+		case 1:
+			return elevation;
+		case 2:
+			return azumiuth;
+		default:
+			return 0;
+		}
+
+	}
+
+	// public double getRotationBank() {
+	//
+	// return getRotAngle(0) ;
+	//
+	// }
+
+	// public double getRotationAttitude() {
+	//
+	// return getRotAngle(2);
+	// }
+	//
+	// public double getRotationHeading() {
+	//
+	// return getRotAngle(1) ;
+	// }
 
 	/**
 	 * Gets the rotation tilt.
@@ -324,7 +554,7 @@ public class RotationNR {
 	 */
 	public double getRotationTilt() {
 
-		return storage.getAngles(getOrder(), getConvention())[0];
+		return getRotAngle(0);
 
 	}
 
@@ -335,7 +565,7 @@ public class RotationNR {
 	 */
 	public double getRotationElevation() {
 
-		return storage.getAngles(getOrder(), getConvention())[1];
+		return getRotAngle(1);
 	}
 
 	/**
@@ -345,8 +575,42 @@ public class RotationNR {
 	 */
 	public double getRotationAzimuth() {
 
-		return storage.getAngles(getOrder(), getConvention())[2];
+		return getRotAngle(2);
 	}
+
+	/**
+	 * Gets the rotation x.
+	 *
+	 * @return the rotation x
+	 */
+//	@Deprecated // use getRotationBank()
+//	public double getRotationX() {
+//
+//		return getRotAngle(0);
+//
+//	}
+
+	/**
+	 * Gets the rotation y.
+	 *
+	 * @return the rotation y
+	 */
+//	@Deprecated // use getRotationAttitude()
+//	public double getRotationY() {
+//
+//		return getRotAngle(2);
+//	}
+
+	/**
+	 * Gets the rotation z.
+	 *
+	 * @return the rotation z
+	 */
+//	@Deprecated // use getRotationHeading()
+//	public double getRotationZ() {
+//
+//		return getRotAngle(1);
+//	}
 
 	/**
 	 * Gets the rotation matrix2 quaturnion w.
@@ -354,7 +618,10 @@ public class RotationNR {
 	 * @return the rotation matrix2 quaturnion w
 	 */
 	public double getRotationMatrix2QuaturnionW() {
-		return storage.getQ0();
+		double temp = 0.5 * Math.sqrt(1 + rotationMatrix[0][0] + rotationMatrix[1][1] + rotationMatrix[2][2]);
+		if (temp > 1)
+			throw new RuntimeException("Matrix needs normalization");
+		return temp;
 	}
 
 	/**
@@ -363,7 +630,8 @@ public class RotationNR {
 	 * @return the rotation matrix2 quaturnion x
 	 */
 	public double getRotationMatrix2QuaturnionX() {
-		return storage.getQ1();
+		double temp = 0.5 * Math.sqrt(1 + rotationMatrix[0][0] + rotationMatrix[1][1] + rotationMatrix[2][2]);
+		return (rotationMatrix[2][1] - rotationMatrix[1][2]) * 0.25 / temp;
 	}
 
 	/**
@@ -372,7 +640,8 @@ public class RotationNR {
 	 * @return the rotation matrix2 quaturnion y
 	 */
 	public double getRotationMatrix2QuaturnionY() {
-		return storage.getQ2();
+		double temp = 0.5 * Math.sqrt(1 + rotationMatrix[0][0] + rotationMatrix[1][1] + rotationMatrix[2][2]);
+		return (rotationMatrix[0][2] - rotationMatrix[2][0]) * 0.25 / temp;
 	}
 
 	/**
@@ -381,23 +650,8 @@ public class RotationNR {
 	 * @return the rotation matrix2 quaturnion z
 	 */
 	public double getRotationMatrix2QuaturnionZ() {
-		return storage.getQ3();
-	}
-
-	public static  RotationOrder getOrder() {
-		return order;
-	}
-
-	public static  void setOrder(RotationOrder o) {
-		order = o;
-	}
-
-	public static RotationConvention getConvention() {
-		return convention;
-	}
-
-	public static void setConvention(RotationConvention convention) {
-		RotationNR.convention = convention;
+		double temp = 0.5 * Math.sqrt(1 + rotationMatrix[0][0] + rotationMatrix[1][1] + rotationMatrix[2][2]);
+		return (rotationMatrix[1][0] - rotationMatrix[0][1]) * 0.25 / temp;
 	}
 
 }
--- test/java/src/junit/test/neuronrobotics/utilities/RotationNRTest.java
+++ test/java/src/junit/test/neuronrobotics/utilities/RotationNRTest.java
@@ -28,83 +28,99 @@ public class RotationNRTest {
 
 	/**
 	 * Test.
-	 * @throws FileNotFoundException 
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	@Test
 	public void test() throws FileNotFoundException {
 		int failCount = 0;
-		int iterations = 10;
-		RotationOrder[] list = { RotationOrder.XYZ
-				
-		};
-		RotationConvention[] conventions = {  RotationConvention.VECTOR_OPERATOR };
+		int iterations = 100;
+		RotationOrder[] list = {  RotationOrder.XYZ
+									// RotationOrder.XZY,
+									// RotationOrder.YXZ,
+									// RotationOrder.YZX,
+				//RotationOrder.ZXY, RotationOrder.ZYX, RotationOrder.XYX, RotationOrder.XZX, RotationOrder.YXY,
+				//RotationOrder.YZY, RotationOrder.ZXZ, RotationOrder.ZYZ 
+				};
+		RotationConvention[] conventions = { RotationConvention.VECTOR_OPERATOR };
 		for (RotationConvention conv : conventions) {
-			RotationNR.setConvention(conv);
+			// RotationNR.setConvention(conv);
 			System.out.println("\n\nUsing convention " + conv.toString());
 			for (RotationOrder ro : list) {
-				RotationNR.setOrder(ro);
+				// RotationNR.setOrder(ro);
 				System.out.println("\n\nUsing rotationOrder " + ro.toString());
 				failCount = 0;
 				for (int i = 0; i < iterations; i++) {
-					double tilt = Math.toRadians((Math.random() * 359) - 179.5);
-					double elevation = Math.toRadians((Math.random() * 180) - 90);
-					double azumus = Math.toRadians((Math.random() * 359) - 179.5);
-					RotationNR rotTest = new RotationNR(Math.toDegrees(tilt), Math.toDegrees(azumus),
-							Math.toDegrees(elevation));
-					System.out.println("\n\nTest #" + i);
-					System.out.println("Testing Az=" + Math.toDegrees(azumus) + " El=" + Math.toDegrees(elevation)
-							+ " Tl=" + Math.toDegrees(tilt));
-					System.out.println("Got     Az=" + Math.toDegrees(rotTest.getRotationAzimuth()) + " El="
-							+ Math.toDegrees(rotTest.getRotationElevation()) + " Tl="
-							+ Math.toDegrees(rotTest.getRotationTilt()));
+					double tilt = Math.toRadians((Math.random() * 360) - 180);
+					double elevation = Math.toRadians((Math.random() * 360) - 180);
+					double azumus = Math.toRadians((Math.random() * 360) - 180);
+					try {
+						RotationNR rotTest = new RotationNR(Math.toDegrees(tilt), Math.toDegrees(azumus),
+								Math.toDegrees(elevation));
+						System.out.println("\n\nTest #" + i);
+						System.out.println("Testing Az=" + Math.toDegrees(azumus) + " El=" + Math.toDegrees(elevation)
+								+ " Tl=" + Math.toDegrees(tilt));
+						System.out.println("Got     Az=" + Math.toDegrees(rotTest.getRotationAzimuth()) + " El="
+								+ Math.toDegrees(rotTest.getRotationElevation()) + " Tl="
+								+ Math.toDegrees(rotTest.getRotationTilt()));
 
-					if (!RotationNR.bound(tilt - .01, tilt + .01, rotTest.getRotationTilt())) {
-						failCount++;
-						System.err.println("Rotation Tilt is not consistant. expected " + Math.toDegrees(tilt) + " got "
-								+ Math.toDegrees(rotTest.getRotationTilt()) + " \t\tOff By "
-								+ (Math.toDegrees(tilt) - Math.toDegrees(rotTest.getRotationTilt())));
-					}
-					if (!RotationNR.bound(elevation - .01, elevation + .01, rotTest.getRotationElevation())) {
-						failCount++;
-						System.err.println("Rotation Elevation is not consistant. expected " + Math.toDegrees(elevation)
-								+ " got " + Math.toDegrees(rotTest.getRotationElevation()) + " \t\tOff By "
-								+ (Math.toDegrees(elevation) + Math.toDegrees(rotTest.getRotationElevation()))
+						if (!RotationNR.bound(tilt - .01, tilt + .01, rotTest.getRotationTilt())) {
+							failCount++;
+							System.err.println("Rotation Tilt is not consistant. expected " + Math.toDegrees(tilt)
+									+ " got " + Math.toDegrees(rotTest.getRotationTilt()) + " \t\tOff By "
+									+ (Math.toDegrees(tilt) - Math.toDegrees(rotTest.getRotationTilt())));
+						}
+						if (!RotationNR.bound(elevation - .01, elevation + .01, rotTest.getRotationElevation())) {
+							failCount++;
+							System.err.println("Rotation Elevation is not consistant. expected "
+									+ Math.toDegrees(elevation) + " got "
+									+ Math.toDegrees(rotTest.getRotationElevation()) + " \t\tOff By "
+									+ (Math.toDegrees(elevation) + Math.toDegrees(rotTest.getRotationElevation()))
 
-						);
+							);
+						}
+						if (!RotationNR.bound(azumus - .01, azumus + .01, rotTest.getRotationAzimuth())) {
+							failCount++;
+							System.err.println("Rotation azumus is not consistant. expected " + Math.toDegrees(azumus)
+									+ " got " + Math.toDegrees(rotTest.getRotationAzimuth()) + " \t\tOff By "
+									+ (Math.toDegrees(azumus) - Math.toDegrees(rotTest.getRotationAzimuth())));
+						}
+						ThreadUtil.wait(20);
+					} catch (NumberFormatException ex) {
+						if(elevation >=Math.PI/2 || elevation <=-Math.PI/2){
+							System.out.println("Invalid numbers rejected ok");
+						}
 					}
-					if (!RotationNR.bound(azumus - .01, azumus + .01, rotTest.getRotationAzimuth())) {
-						failCount++;
-						System.err.println("Rotation azumus is not consistant. expected " + Math.toDegrees(azumus)
-								+ " got " + Math.toDegrees(rotTest.getRotationAzimuth()) + " \t\tOff By "
-								+ (Math.toDegrees(azumus) - Math.toDegrees(rotTest.getRotationAzimuth())));
-					}
-					ThreadUtil.wait(20);
+
 				}
 				if (failCount < 1) {
-					System.out.println("Orentation " + ro.toString() + " worked ina all cases");
+					System.out.println("Orentation " + ro.toString() + " worked in all cases");
 
 				}
+				// frame();
+				// frame2();
+				System.out.println("Frame test passed with " + ro);
+				//return;
 			}
 		}
-		new RotationNR(0.38268343236509234, -1.2443977214448087E-17, 2.1758644300923683E-16, -0.9238795325112857);
+
 		File f = new File("carlRobot.xml");
 		if (f.exists()) {
 			MobileBase pArm = new MobileBase(new FileInputStream(f));
-			try{
+			try {
 				String xmlParsed = pArm.getXml();
 				BufferedWriter writer = null;
-	
+
 				writer = new BufferedWriter(new FileWriter("carlRobot2.xml"));
 				writer.write(xmlParsed);
-	
+
 				if (writer != null)
 					writer.close();
-				
-			}catch(Exception ex){
+
+			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 			pArm.disconnect();
-			System.exit(0);
 		}
 		if (failCount > 1) {
 			fail("Rotation failed " + failCount + " times of " + ((iterations * 3 * list.length) - 0));
@@ -112,4 +128,57 @@ public class RotationNRTest {
 		}
 	}
 
+//	public void frame() {
+//		double w = 0.25021580750394473;
+//		double x = -0.5895228206035708;
+//		double y = 0.12359002177935843;
+//		double z = 0.758010817983053;
+//		RotationNR knownAngles = new RotationNR(w, x, y, z);
+//		assertArrayEquals(new double[] { w, x, y, z },
+//				new double[] { knownAngles.getRotationMatrix2QuaturnionW(), knownAngles.getRotationMatrix2QuaturnionX(),
+//						knownAngles.getRotationMatrix2QuaturnionY(), knownAngles.getRotationMatrix2QuaturnionZ() },
+//				0.0000001);
+//		double knownTilt = Math.toDegrees(knownAngles.getRotationTilt());
+//		double knownAz = Math.toDegrees(knownAngles.getRotationAzimuth());
+//		double knownel = Math.toDegrees(knownAngles.getRotationElevation());
+//		System.out.println("Known angles are az=" + knownAz + " el=" + knownel + " tilt=" + knownTilt);
+//
+//		RotationNR doubleCheck = new RotationNR(knownTilt, knownAz, knownel);
+//		assertArrayEquals(
+//				new double[] { doubleCheck.getRotationMatrix2QuaturnionW(), doubleCheck.getRotationMatrix2QuaturnionX(),
+//						doubleCheck.getRotationMatrix2QuaturnionY(), doubleCheck.getRotationMatrix2QuaturnionZ() },
+//				new double[] { knownAngles.getRotationMatrix2QuaturnionW(), knownAngles.getRotationMatrix2QuaturnionX(),
+//						knownAngles.getRotationMatrix2QuaturnionY(), knownAngles.getRotationMatrix2QuaturnionZ() },
+//				0.0000001);
+//
+//		assertArrayEquals(new double[] { knownTilt, knownel, knownAz }, new double[] { -111.422, -72.858, 37.570 },
+//				0.01);
+//	}
+//
+//	public void frame2() {
+//		double w = 0.29405190560732924;
+//		double x = 0.5230342577988376;
+//		double y = -0.32364491993997213;
+//		double z = 0.7315890976323846;
+//		RotationNR knownAngles = new RotationNR(w, x, y, z);
+//		assertArrayEquals(new double[] { w, x, y, z },
+//				new double[] { knownAngles.getRotationMatrix2QuaturnionW(), knownAngles.getRotationMatrix2QuaturnionX(),
+//						knownAngles.getRotationMatrix2QuaturnionY(), knownAngles.getRotationMatrix2QuaturnionZ() },
+//				0.0000001);
+//		double knownTilt = Math.toDegrees(knownAngles.getRotationTilt());
+//		double knownAz = Math.toDegrees(knownAngles.getRotationAzimuth());
+//		double knownel = Math.toDegrees(knownAngles.getRotationElevation());
+//		System.out.println("Known angles are az=" + knownAz + " el=" + knownel + " tilt=" + knownTilt);
+//
+//		RotationNR doubleCheck = new RotationNR(knownTilt, knownAz, knownel);
+//		assertArrayEquals(
+//				new double[] { doubleCheck.getRotationMatrix2QuaturnionW(), doubleCheck.getRotationMatrix2QuaturnionX(),
+//						doubleCheck.getRotationMatrix2QuaturnionY(), doubleCheck.getRotationMatrix2QuaturnionZ() },
+//				new double[] { knownAngles.getRotationMatrix2QuaturnionW(), knownAngles.getRotationMatrix2QuaturnionX(),
+//						knownAngles.getRotationMatrix2QuaturnionY(), knownAngles.getRotationMatrix2QuaturnionZ() },
+//				0.0000001);
+//
+//		assertArrayEquals(new double[] { knownTilt, knownel, knownAz }, new double[] { 55.711, 107.132, -108.137 },
+//				0.01);
+//	}
 }
