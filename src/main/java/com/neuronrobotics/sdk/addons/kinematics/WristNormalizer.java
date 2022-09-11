4b4a186167872c19cf5fe9c514374a0171ca2d35
Kevin Harrington <mad.hephaestus@gmail.com>
2022-09-11 17:32:33 -0400

    more wrist normalization options

--- src/main/java/com/neuronrobotics/sdk/addons/kinematics/WristNormalizer.java
+++ src/main/java/com/neuronrobotics/sdk/addons/kinematics/WristNormalizer.java
@@ -3,20 +3,21 @@ package com.neuronrobotics.sdk.addons.kinematics;
 import java.util.HashMap;
 
 public class WristNormalizer {
-	private static boolean strictMode = false;
+	private static boolean strictMode = true;
 	
 	public static double[] normalize(double[] calculated, double[] current, DHChain chain) {
 		AbstractKinematicsNR kin = chain.kin;
 		// DecimalFormat df = new DecimalFormat("000.00");
-		double[] alt1 = new double[] { calculated[0] - 180, -calculated[1], calculated[2] - 180 };
-		double[] calculated2 = new double[] { calculated[0] + 360, calculated[1] + 360, calculated[2] + 360 };
-		double[] calculated3 = new double[] { calculated[0] - 360, calculated[1] - 360, calculated[2] - 360 };
-		double[] alt2 = new double[] { alt1[0] + 360, alt1[1] + 360, alt1[2] + 360 };
-		double[] alt3 = new double[] { alt1[0] - 360, alt1[1] - 360, alt1[2] - 360 };
-		double[] calculated6 = new double[] { calculated[0] - 360, calculated[1], calculated[2] };
-		double[] calculated7 = new double[] { calculated[0] + 360, calculated[1], calculated[2] };
-		double[] als4 = new double[] { alt1[0] - 360, alt1[1], alt1[2] };
-		double[] alt5 = new double[] { alt1[0] + 360, alt1[1], alt1[2] };
+		double[] alt1 =option( calculated[0] - 180, -calculated[1], calculated[2] - 180 );
+		double[] calculated2 =option( calculated[0] + 360, calculated[1] + 360, calculated[2] + 360 );
+		double[] calculated3 =option( calculated[0] - 360, calculated[1] - 360, calculated[2] - 360 );
+		double[] alt2 =option( alt1[0] + 360, alt1[1] + 360, alt1[2] + 360 );
+		double[] alt3 =option( alt1[0] - 360, alt1[1] - 360, alt1[2] - 360 );
+		double[] calculated6 =option( calculated[0] - 360, calculated[1], calculated[2] );
+		double[] calculated7 =option( calculated[0] + 360, calculated[1], calculated[2] );
+		double[] als4 =option( alt1[0] - 360, alt1[1], alt1[2] );
+		double[] alt5 =option( alt1[0] + 360, alt1[1], alt1[2] );
+		
 		
 		
 		HashMap<double[], Double> scores = new HashMap<>();
@@ -30,6 +31,11 @@ public class WristNormalizer {
 		score(calculated7, current, scores, kin);
 		score(als4, current, scores, kin);
 		score(alt5, current, scores, kin);
+		score(option( calculated[0] -180, -calculated[1], calculated[2]+180 ), current, scores, kin);
+		score(option( alt1[0] -180, alt1[1], -alt1[2]+180 ), current, scores, kin);
+		score(option( calculated[0] +180, -calculated[1], calculated[2]-180 ), current, scores, kin);
+		score(option( alt1[0] +180, -alt1[1], alt1[2]-180 ), current, scores, kin);
+		
 		if (scores.size() > 0) {
 			double score = scores.get(calculated);
 			double[] ret = calculated;
@@ -48,7 +54,10 @@ public class WristNormalizer {
 			return current;
 		throw new RuntimeException("No Wrist Solution! ");
 	}
-
+	private static double[] option(double w1,double w2,double w3) {
+		return new double[] {w1,w2,w3}; 
+	}
+	
 	private static void score(double[] calculated, double[] current, HashMap<double[], Double> scores,
 			AbstractKinematicsNR kin) {
 		double delt = 0;
@@ -57,12 +66,10 @@ public class WristNormalizer {
 			calculated[i] = calculated[i] % 360;
 			
 			if (calculated[i] > kin.getMaxEngineeringUnits(i3)) {
-				if(strictMode)return;
-				calculated[i]=kin.getMaxEngineeringUnits(i3);
+				return;
 			}
 			if (calculated[i] < kin.getMinEngineeringUnits(i3)) {
-				if(strictMode)return;
-				calculated[i]=kin.getMinEngineeringUnits(i3);
+				return;
 			}
 			double measure = current[i] - calculated[i];
 			if (Math.abs(measure) > Math.abs(delt)) {
