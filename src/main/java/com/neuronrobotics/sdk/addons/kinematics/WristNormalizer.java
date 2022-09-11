4a2f97b85c381f69cf79cfae2ad138eb82713808
Kevin Harrington <mad.hephaestus@gmail.com>
2022-09-11 15:41:09 -0400

    updated the wrist normalizer method

--- src/main/java/com/neuronrobotics/sdk/addons/kinematics/WristNormalizer.java
+++ src/main/java/com/neuronrobotics/sdk/addons/kinematics/WristNormalizer.java
@@ -11,12 +11,24 @@ public class WristNormalizer {
 		HashMap<double[],Double> scores= new HashMap<>();
 		score(calculated,current,scores,kin);
 		score(alt1,current,scores,kin);
+		
+		double[] calculated2 = new double[] {calculated[0]+360,calculated[1]+360,calculated[2]+360};
+		score(calculated2,current,scores,kin);
+		double[] calculated3 = new double[] {calculated[0]-360,calculated[1]-360,calculated[2]-360};
+		score(calculated3,current,scores,kin);
+		double[] calculated4 = new double[] {alt1[0]+360,alt1[1]+360,alt1[2]+360};
+		score(calculated4,current,scores,kin);
+		double[] calculated5 = new double[] {alt1[0]-360,alt1[1]-360,alt1[2]-360};
+		score(calculated5,current,scores,kin);
+		double[] calculated6 = new double[] {calculated[0]-360,calculated[1],calculated[2]};
+		score(calculated6,current,scores,kin);
+		double[] calculated7 = new double[] {calculated[0]+360,calculated[1],calculated[2]};
+		score(calculated7,current,scores,kin);
 
-		score(new double[] {calculated[0]-360,calculated[1],calculated[2]},current,scores,kin);
-		score(new double[] {calculated[0]+360,calculated[1],calculated[2]},current,scores,kin);
-
-		score(new double[] {alt1[0]-360,alt1[1],alt1[2]},current,scores,kin);
-		score(new double[] {alt1[0]+360,alt1[1],alt1[2]},current,scores,kin);
+		double[] calculated8 = new double[] {alt1[0]-360,alt1[1],alt1[2]};
+		score(calculated8,current,scores,kin);
+		double[] calculated9 = new double[] {alt1[0]+360,alt1[1],alt1[2]};
+		score(calculated9,current,scores,kin);
 		
 		double score=scores.get(calculated);
 		double[] ret=calculated;
@@ -35,11 +47,12 @@ public class WristNormalizer {
 		double delt=0;
 		for(int i=0;i<3;i++) {
 			int i3 = i+3;
+			calculated[i]=calculated[i] % 360;
 			if(calculated[i] >kin.getMaxEngineeringUnits(i3)) {
-				calculated[i]-=360;
+				return;
 			}
 			if(calculated[i] <kin.getMinEngineeringUnits(i3)) {
-				calculated[i]+=360;
+				return;
 			}
 			double measure = current[i]-calculated[i];
 			if(Math.abs(measure)>Math.abs(delt)) {
