6a6a069aa4cbfacb9eb203a943ed169088c3a7ee
Kevin Harrington <mad.hephaestus@gmail.com>
2024-03-24 11:50:59 -0400

    fully robust Unit test that checks loaded XML against exported XML

--- test/java/src/junit/test/neuronrobotics/utilities/TestMobilBaseLoading.java
+++ test/java/src/junit/test/neuronrobotics/utilities/TestMobilBaseLoading.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
@@ -13,11 +15,21 @@ import com.neuronrobotics.sdk.addons.kinematics.MobileBase;
 public class TestMobilBaseLoading {
 
 	@Test
-	public void test() throws FileNotFoundException {
-		MobileBase base = new MobileBase(new FileInputStream(new File("src/main/resources/com/neuronrobotics/sdk/addons/kinematics/xml/NASASuspensionTest.xml")));
+	public void test() throws IOException {
+		File file = new File("src/main/resources/com/neuronrobotics/sdk/addons/kinematics/xml/NASASuspensionTest.xml");
+		
+		String content = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+		MobileBase base = new MobileBase(new FileInputStream(file));
 		
 		if(Math.abs(0.1-base.getMassKg())>0.0001) {
 			fail("Base mass failed to load! expected "+0.1+" got "+base.getMassKg());
+		}
+		String read = base.getXml();
+		if(!content.contentEquals(read)) {
+			File out = new File("src/main/resources/com/neuronrobotics/sdk/addons/kinematics/xml/NASASuspensionTestOUTPUT.xml");
+			Files.write( Paths.get(out.getAbsolutePath()), read.getBytes());
+			System.out.println("diff "+file.getAbsolutePath()+" "+out.getAbsolutePath());
+			fail("What was loaded failed to match the source");
 		}
 	}
 
